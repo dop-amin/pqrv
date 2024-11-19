1cbfc85634250ed57c0d9c1dc9e55e40c117a5b7
Amin Abdulrahman <amin@abdulrahman.de>
2024-11-19 09:58:54 -0500

    Update nix flake to provide glibc for static linking

--- flake.nix
+++ flake.nix
@@ -1,13 +1,30 @@
 {
-  description = "pqrv";
+  description = "A flake to set up a development shell with the RISC-V 64 GNU toolchain";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-  };
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.gcc14 = nixpkgs.legacyPackages.x86_64-linux.gcc-unwrapped.overrideAttrs (oldAttrs: rec {
-      version = "14"; # Specify the desired version if available
-    });
-  };
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          crossSystem = {
+            config = "riscv64-unknown-linux-gnu";
+          };
+        };
+    in
+    with pkgs;
+    {
+      devShells.${system}.default = stdenv.mkDerivation
+        {
+          name = "RISC-V 64 Toolchain";
+
+          nativeBuildInputs = [
+          ];
+
+          buildInputs = [
+            glibc.static
+          ];
+        };
+    };
 }
