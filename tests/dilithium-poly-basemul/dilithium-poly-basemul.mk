b68c119bc4d4ad965fa74bed8d2c425f628a96d3
Amin Abdulrahman <amin@abdulrahman.de>
2025-06-27 05:31:06 -0400

    refactor: Rename poly-basemul test to dilithium-poly-basemul

---
+++ tests/dilithium-poly-basemul/dilithium-poly-basemul.mk
@@ -0,0 +1,18 @@
+# Test name - needs to match the directory name
+TESTS += dilithium-poly-basemul
+
+# All further variables must be prefixed with the capitalized test name
+
+# Platforms this test should run on (matching the directory name in envs/)
+DILITHIUM_POLY_BASEMUL_PLATFORMS += cross-rvv
+DILITHIUM_POLY_BASEMUL_PLATFORMS += cross-rv64im
+
+# C sources required for this test
+DILITHIUM_POLY_BASEMUL_SOURCES += main.c
+
+# Assembly sources required for this test
+DILITHIUM_POLY_BASEMUL_ASM_DIR = ../../asm/manual/dilithium_poly_basemul
+DILITHIUM_POLY_BASEMUL_ASMS += $(DILITHIUM_POLY_BASEMUL_ASM_DIR)/poly_basemul_8l_acc_rv64im.s
+DILITHIUM_POLY_BASEMUL_ASMS += $(DILITHIUM_POLY_BASEMUL_ASM_DIR)/poly_basemul_8l_acc_rv64im_opt_c908.s
+DILITHIUM_POLY_BASEMUL_ASMS += $(DILITHIUM_POLY_BASEMUL_ASM_DIR)/poly_basemul_8l_acc_dual_rv64im.s
+
