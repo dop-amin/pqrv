fe682e4d84e7585b0e6658c8c88021999c806bf2
Justus Bergermann <justus.bergermann@mpi-sp.org>
2026-01-28 15:08:22 +0100

    wip: optimizing ntt w/ barret mul

--- envs/common/cross.mk
+++ envs/common/cross.mk
@@ -1,11 +1,13 @@
 # emulation (qemu or empty for native)
 EMU = qemu-riscv64
 CC = riscv64-unknown-linux-gnu-gcc
+REMOTE_USER = justus.bergerman
+_USER = juber
 SSH_DEST = root@k230-0
 SSH_PATH = /home
-SSH_DEST2 = justus.bergermann@bpi-f3-0
-SSH_PATH2 = /home/justus.bergerman/uarch_benchmarks
+SSH_DEST2 = $(REMOTE_USER)@bpi-f3-0
+SSH_PATH2 = /home/$(REMOTE_USER)/uarch_benchmarks
 
-SSH_ID = /home/juber/.ssh/id_ed25519
-SSH_CONF = /home/juber/.ssh/config
+SSH_ID = /home/$(_USER)/.ssh/id_ed25519
+SSH_CONF = /home/$(_USER)/.ssh/config
 include ../common/common.mk
